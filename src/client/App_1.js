8906ebe7407e7a25dd4d0e745a0be0516d118605
Konstantin Belousov <kombuchamp@gmail.com>
2019-03-04 15:29:41 +0300

    Add baloon

--- src/client/App_1.js
+++ src/client/App_1.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
-import { YMaps, Map } from 'react-yandex-maps';
+import { YMaps, Map, Placemark } from 'react-yandex-maps';
 
 export class App_1 extends Component {
     render() {
-        const apikey = `9d4c59f1-72a1-418f-a219-a1734042cd50`; 
+        const apikey = `9d4c59f1-72a1-418f-a219-a1734042cd50`;
         // Works somehow without api key
         const state = {
             center: [55.75, 37.57],
-            zoom: 9
-        }
+            zoom: 9,
+            controls: ['zoomControl', 'fullscreenControl'],
+        };
+
         return (
             <div>
                 <h1>react-yandex-maps</h1>
-                <YMaps query={{ apikey}}>
-                    <Map defaultState={state}  width='100%' height='100vh'/>
+                <YMaps query={{ apikey, load: 'package.full' }}>
+                    <Map defaultState={state} width="100%" height="100vh">
+                        <Placemark
+                            defaultGeometry={[55.75, 37.57]}
+                            properties={{
+                                balloonContentBody: `<h3>BALOON</h3><input type="button" value="OK"/><script>alert('?')</script>`,
+                            }}
+                        />
+                    </Map>
                 </YMaps>
             </div>
         );
