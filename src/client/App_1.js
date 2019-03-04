6d0f57ac54b38499f80b0bdc757c3585670884e0
Konstantin Belousov <kombuchamp@gmail.com>
2019-03-04 14:16:53 +0300

    Add Map

--- src/client/App_1.js
+++ src/client/App_1.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
+import { YMaps, Map } from 'react-yandex-maps'
 
 export class App_1 extends Component {
   render() {
+    const apikey = `9d4c59f1-72a1-418f-a219-a1734042cd50`;
+
     return (
       <div>
         <h1>react-yandex-maps</h1>
+      <YMaps query={{apikey}}>
+          <Map defaultState={{  center: [55.75, 37.57], zoom: 9 }} />
+      </YMaps>
       </div>
     )
   }
