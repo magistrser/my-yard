743728e1d1429186e71bcfd1a00da90f67aa2faf
Konstantin Belousov <kombuchamp@gmail.com>
2019-03-04 14:30:36 +0300

    Add width and height props to map

--- src/client/App_1.js
+++ src/client/App_1.js
@@ -1,19 +1,23 @@
-import React, { Component } from 'react'
-import { YMaps, Map } from 'react-yandex-maps'
+import React, { Component } from 'react';
+import { YMaps, Map } from 'react-yandex-maps';
 
 export class App_1 extends Component {
-  render() {
-    const apikey = `9d4c59f1-72a1-418f-a219-a1734042cd50`;
-
-    return (
-      <div>
-        <h1>react-yandex-maps</h1>
-      <YMaps query={{apikey}}>
-          <Map defaultState={{  center: [55.75, 37.57], zoom: 9 }} />
-      </YMaps>
-      </div>
-    )
-  }
+    render() {
+        const apikey = `9d4c59f1-72a1-418f-a219-a1734042cd50`; 
+        // Works somehow without api key
+        const state = {
+            center: [55.75, 37.57],
+            zoom: 9
+        }
+        return (
+            <div>
+                <h1>react-yandex-maps</h1>
+                <YMaps query={{ apikey}}>
+                    <Map defaultState={state}  width='100%' height='100vh'/>
+                </YMaps>
+            </div>
+        );
+    }
 }
 
-export default App_1
+export default App_1;
