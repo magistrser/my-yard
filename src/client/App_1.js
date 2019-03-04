2d4a95750869a913856d0344b019221fe8993922
Konstantin Belousov <kombuchamp@gmail.com>
2019-03-04 12:27:20 +0300

    Add App_1 component

---
+++ src/client/App_1.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class App_1 extends Component {
+  render() {
+    return (
+      <div>
+        <h1>react-yandex-maps</h1>
+      </div>
+    )
+  }
+}
+
+export default App_1
--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import App_1 from './App_1';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App_1 />, document.getElementById('root'));
