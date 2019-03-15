94c35b7ec486c357067ffdba007e3b309de0ca80
Konstantin Belousov <kombuchamp@gmail.com>
2019-03-15 14:37:36 +0300

    [MYYAR-3] Add some more styles

--- src/client/components/GeolocationMap/GeolocationMap.js
+++ src/client/components/GeolocationMap/GeolocationMap.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { YMaps, Map, Placemark } from 'react-yandex-maps';
-import './GeolocationMap.css'
+import './GeolocationMap.css';
 
 export class GeolocationMap extends Component {
     constructor() {
@@ -8,35 +8,33 @@ export class GeolocationMap extends Component {
         this.state = {
             center: [55.771707, 37.678784],
             zoom: 7,
-            controls: ['zoomControl', 'fullscreenControl']
+            controls: ['zoomControl', 'fullscreenControl'],
         };
         this.apikey = `9d4c59f1-72a1-418f-a219-a1734042cd50`;
     }
 
     onMapClick = async e => {
-      let result = await this.ymapsAPI.geolocation.get({provider:"auto"}); // "yandex", "browser" or "auto"
-      console.log("Geolocation: ",result); // Geolocation objects
-      let countryName = result.geoObjects.get(0).getCountry();
-      document.querySelector('div .overlay').innerHTML = `<p>${countryName}</p>`;
-    }
-    
+        let result = await this.ymapsAPI.geolocation.get({ provider: 'auto' }); // "yandex", "browser" or "auto"
+        console.log('Geolocation: ', result); // Geolocation objects
+        let countryName = result.geoObjects.get(0).getCountry();
+        document.querySelector('div .overlay').innerHTML = `<p>${countryName}</p>`;
+    };
+
     render() {
         return (
             <>
-                <div className="overlay">
-                </div>
-                <div className="map-container">
-                    <YMaps query={{ apikey: this.apikey, load: 'package.full' }} >
-                        <Map
-                            onClick={this.onMapClick}
-                            onLoad={ymaps => this.ymapsAPI = ymaps} // Keep ymaps api instance as a field
-                            instanceRef={map => this.mapInstance = map} // Keep map instance as a field
-                            state={this.state}
-                            width="100%"
-                            height="100vh"
-                        />
-                    </YMaps>
-                </div>
+                <div className="overlay" style={{ display: 'none' }} />
+                <YMaps query={{ apikey: this.apikey, load: 'package.full' }}>
+                    <Map
+                        className="map-container"
+                        onClick={this.onMapClick}
+                        onLoad={ymaps => (this.ymapsAPI = ymaps)} // Keep ymaps api instance as a field
+                        instanceRef={map => (this.mapInstance = map)} // Keep map instance as a field
+                        state={this.state}
+                        //width="100%"
+                        //height="100vh"
+                    />
+                </YMaps>
             </>
         );
     }
