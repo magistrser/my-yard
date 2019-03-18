00ad1fbe7cb5103b7276f388c04750b642071754
Konstantin Belousov <kombuchamp@gmail.com>
2019-03-18 13:31:01 +0300

    [MYYAR-6] Add UserClickMap example

---
+++ src/examples/UserClickMap/UserClickMap.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { YMaps, Map, Placemark, withYMaps } from 'react-yandex-maps';
+
+export default class UserClickMap extends Component {
+    render() {
+        return (
+            <div>
+                <aside id="user-click-aside">1</aside>
+                <main id="user-click-main">1</main>
+            </div>
+        );
+    }
+}
