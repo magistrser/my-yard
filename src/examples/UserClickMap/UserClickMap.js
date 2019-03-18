4e2db28492f4f183d5a92c3fe6ddd44c1daae085
Konstantin Belousov <kombuchamp@gmail.com>
2019-03-18 14:33:28 +0300

    [MYYAR-6] Add placemark adding functionality

--- src/examples/UserClickMap/UserClickMap.js
+++ src/examples/UserClickMap/UserClickMap.js
@@ -6,23 +6,78 @@ export default class UserClickMap extends Component {
     constructor() {
         super();
         this.state = {
+            // Map state
             center: [55.771707, 37.678784],
             zoom: 7,
             controls: ['zoomControl', 'fullscreenControl', 'geolocationControl'],
+            // Component state
         };
         this.apikey = `9d4c59f1-72a1-418f-a219-a1734042cd50`;
+        this.isInAddMode = false;
     }
+
+    handleAddNewPlacemarkClick = ev => {
+        // Change cursor depending on
+        this.isInAddMode = !this.isInAddMode;
+        if (this.isInAddMode) {
+            this.cursor.setKey('crosshair');
+        } else {
+            this.cursor.setKey('grab');
+        }
+    };
+
+    handleMapClick = ev => {
+        if (this.isInAddMode) {
+            ev.preventDefault();
+            this.addPlaceMark(ev);
+            //this.setState(); // No need to set state, map refreshes itself
+        }
+    };
+
+    addPlaceMark = ev => {
+        // Add placemark using api directly
+        const coords = ev.get('coords');
+        const placemark = new this.ymapsAPI.Placemark(
+            coords,
+            {
+                iconContent: `${coords}`,
+            },
+            {
+                preset: 'islands#greenStretchyIcon',
+            }
+        );
+        this.mapInstance.geoObjects.add(placemark);
+    };
+
+    handleMapLoaded = map => {
+        this.mapInstance = map; // Keep ymaps api instance as a field
+        console.log('Maps instance', map);
+        // Add managed cursor and save it as a field
+        // https://tech.yandex.ru/maps/doc/jsapi/2.1/ref/reference/util.cursor.Manager-docpage/
+        this.cursor = map.cursors.push('grab');
+    };
+
+    handleApiLoaded = ymaps => {
+        this.ymapsAPI = ymaps; // Keep map instance as a field
+        console.log('Api instance', ymaps);
+    };
 
     render() {
         return (
             <div>
-                <aside id="user-click-aside">Options</aside>
+                <aside id="user-click-aside">
+                    <p>Options</p>
+                    <button className="user-click-button" id="add-new-placemark" onClick={this.handleAddNewPlacemarkClick}>
+                        Add new placemark
+                    </button>
+                </aside>
                 <main id="user-click-main">
                     <YMaps query={{ apikey: this.apikey, load: 'package.full' }}>
                         <Map
+                            onClick={this.handleMapClick}
                             className="map-container"
-                            onLoad={ymaps => (this.ymapsAPI = ymaps)} // Keep ymaps api instance as a field
-                            instanceRef={map => (this.mapInstance = map)} // Keep map instance as a field
+                            onLoad={this.handleApiLoaded}
+                            instanceRef={this.handleMapLoaded}
                             state={this.state}
                         />
                     </YMaps>
