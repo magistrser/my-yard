6e5c39c4861dde5eec3d1b5688e248902285f6f3
Konstantin Belousov <kombuchamp@gmail.com>
2019-03-19 14:08:39 +0300

    [MYYAR-11] Move folders around

--- src/client/examples/YandexApiExamples/MapUsingApiDirectly/MapUsingApiDirectly.js
+++ src/client/examples/YandexApiExamples/MapUsingApiDirectly/MapUsingApiDirectly.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { ymapLoader } from '../../client/utils/YmapLoader';
+import { ymapLoader } from '../../../utils/YmapLoader';
 
 type Props = {
     //
--- src/client/index.js
+++ src/client/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import ReactDOM from 'react-dom';
-import MapUsingApiDirectly from '../examples/MapUsingApiDirectly/MapUsingApiDirectly';
-import MapUsingApiLibrary from '../examples/MapUsingApiLibrary/MapUsingApiLibrary';
+import MapUsingApiDirectly from './examples/YandexApiExamples/MapUsingApiDirectly/MapUsingApiDirectly';
+import MapUsingApiLibrary from './examples/YandexApiExamples/MapUsingApiLibrary/MapUsingApiLibrary';
 import styles from './index.css';
-import { PlacemarkMap } from '../examples/PlacemarkMap/PacemarkMap';
-import { GeolocationMap } from '../examples/GeolocationMap/GeolocationMap';
-import UserClickMap from '../examples/UserClickMap/UserClickMap';
+import { PlacemarkMap } from './examples/YandexApiExamples/PlacemarkMap/PacemarkMap';
+import { GeolocationMap } from './examples/YandexApiExamples/GeolocationMap/GeolocationMap';
+import UserClickMap from './examples/YandexApiExamples/UserClickMap/UserClickMap';
 
 ReactDOM.render(
     <Router>
