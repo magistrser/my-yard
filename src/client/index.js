267bd3df0c20c6addccd71c3d5db58cb9469fa46
Konstantin Belousov <kombuchamp@gmail.com>
2019-03-19 14:31:05 +0300

    [MYYAR-11] Clean up main application

--- src/client/index.js
+++ src/client/index.js
@@ -1,26 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import ReactDOM from 'react-dom';
-import MapUsingApiDirectly from './examples/YandexApiExamples/MapUsingApiDirectly/MapUsingApiDirectly';
-import MapUsingApiLibrary from './examples/YandexApiExamples/MapUsingApiLibrary/MapUsingApiLibrary';
-import styles from './index.css';
-import { PlacemarkMap } from './examples/YandexApiExamples/PlacemarkMap/PacemarkMap';
-import { GeolocationMap } from './examples/YandexApiExamples/GeolocationMap/GeolocationMap';
-import UserClickMap from './examples/YandexApiExamples/UserClickMap/UserClickMap';
-
 import App from './components/App/App';
-
-// ReactDOM.render(
-//     <Router>
-//         <>
-//             <Route exact path="/" component={MapUsingApiLibrary} />
-//             <Route exact path="/app" component={MapUsingApiDirectly} />
-//             <Route exact path="/placemark-map" component={PlacemarkMap} />
-//             <Route exact path="/geolocation-map" component={GeolocationMap} />
-//             <Route exact path="/user-click-map" component={UserClickMap} />
-//         </>
-//     </Router>,
-//     document.getElementById('root')
-// );
 
 ReactDOM.render(<App />, document.getElementById('root'));
