2dd6a379b97928edea2e09fb854ee0c8ea0b57d4
Konstantin Belousov <kombuchamp@gmail.com>
2019-03-20 16:05:00 +0300

    [MYYAR-8] Fix routes

--- src/client/index.js
+++ src/client/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 import App1 from './components/App1/App1';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 ReactDOM.render(
     <Router>
         <>
-            <Route path="/" exact component={App} />
-            <Route path="/App" component={App1} />
+            <div hidden>There might be navigation bar for all views or smth</div>
+            <Switch>
+                <Route path="/" exact component={App} />
+                <Route path="/App1" component={App1} />
+                <Route component={App /* Redirect to 404 page for instance */} />
+            </Switch>
         </>
     </Router>,
     document.getElementById('root')
