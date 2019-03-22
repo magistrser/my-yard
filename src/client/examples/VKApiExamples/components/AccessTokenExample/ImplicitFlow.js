b306aa4fefd0a13ba8283c016669ccd52325ba97
Konstantin Belousov <kombuchamp@gmail.com>
2019-03-22 17:07:45 +0300

    [MYYAR-8] Add AuthorizationCodeFlow example

--- src/client/examples/VKApiExamples/components/AccessTokenExample/ImplicitFlow.js
+++ src/client/examples/VKApiExamples/components/AccessTokenExample/ImplicitFlow.js
@@ -1,10 +1,23 @@
 /**
+ * Implicit flow is used in client-only authorization
  * https://vk.com/dev/implicit_flow_user
  */
 
 import React from 'react';
 
 export default function ImplicitFlow() {
+    return (
+        <div>
+            <h1>Implicit Flow</h1>
+            <p>
+                Используйте Implicit Flow для вызова методов API ВКонтакте непосредственно с устройства пользователя (например, из
+                Javascript). Ключ доступа, полученный таким способом, не может быть использован для запросов с сервера.
+            </p>
+            <button onClick={handleLoginClick}>Войти через ВКонтакте</button>
+            {outputAnswer()}
+        </div>
+    );
+
     function handleLoginClick(ev) {
         ev.preventDefault();
         location.href =
@@ -13,7 +26,7 @@ export default function ImplicitFlow() {
             '&scope=email' + // Required permissions
             '&redirect_uri=localhost:3000' + // Where to redirect after login (yup, localhost works)
             '&display=popup' + // page, popup or mobile
-            '&response_type=token' + // token or code
+            '&response_type=token' + // different every time
             '&state=arbitrary_string' + // Returned in answer
             '&revoke=1' + // Asks for permissions even if user is already authorized (0 by default)
             '&v=5.92'; // version of api (mandatory)
@@ -34,16 +47,4 @@ export default function ImplicitFlow() {
             );
         });
     }
-
-    return (
-        <div>
-            <h1>Implicit Flow</h1>
-            <p>
-                Используйте Implicit Flow для вызова методов API ВКонтакте непосредственно с устройства пользователя (например, из
-                Javascript). Ключ доступа, полученный таким способом, не может быть использован для запросов с сервера.
-            </p>
-            <button onClick={handleLoginClick}>Войти через ВКонтакте</button>
-            {outputAnswer()}
-        </div>
-    );
 }
