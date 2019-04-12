c15233d0c21949f100baae309db79b40eee8f116
Konstantin Belousov <kombuchamp@gmail.com>
2019-04-12 15:13:59 +0300

    Add few comments

--- examples/InfrastructureExamples/server/index.js
+++ examples/InfrastructureExamples/server/index.js
@@ -14,31 +14,29 @@ import type { $Request, $Response, NextFunction, Middleware } from 'express';
 
 const app = express();
 
-// Post request bodyparser
+/* Body parser middleware */
 app.use(express.json());
 
-// Express session
+/* Session middleware */
 app.use(
     session({
-        secret: 'secret',
+        secret: 'secret', // TODO: Generate guid or something
         resave: true,
         saveUninitialized: true,
     })
 );
 
-// Passport configuration
+/* Passport configuration */
 configurePassport(passport);
-// Passport initialization
 app.use(passport.initialize());
 app.use(passport.session());
 
+/* Serve static files */
 app.use(express.static('dist'));
 
-// Passport auth:
-// First step. Redirects to vk.com for authentication
+/* Passport authentication */
+// VK strat:
 app.get('/api/auth/vkontakte', passport.authenticate('vkontakte'));
-
-// Second step. vk.com redirects back here after authentication
 app.get(
     '/api/auth/vkontakte/callback',
     passport.authenticate('vkontakte', { failureRedirect: '/api/fail' }), // Redirect to /login page for example
@@ -51,6 +49,7 @@ app.get(
     }
 );
 
+/* ROUTES */
 // Endpoint which will respond only if user is authorized
 app.get('/api/restricted-area', ensureAuthenticated, (req, res) => {
     const username = req.user.name;
@@ -78,15 +77,15 @@ app.get('/api/check-authentication', (req, res) => {
 // Logs user out
 app.get('/api/logout', (req, res) => {
     req.logout();
-    res.redirect('/passport');
+    res.redirect('http://localhost:80/');
 });
 
 // Handles any requests that don't match the ones above
 app.get('*', (req, res) => {
-    //res.sendFile(path.resolve(rootFolder, 'dist', 'index.html'));
     res.redirect('http://localhost:80/' + req.query); // HACK: A workaround
 });
 
+/* Start web server */
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
 
 // Middleware that checks if user is authenticated
