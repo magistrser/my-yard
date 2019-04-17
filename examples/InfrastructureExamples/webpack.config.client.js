6d264c2b2c2fd85bfe7ab00ff065e75e1d216a7e
Konstantin Belousov <kombuchamp@gmail.com>
2019-04-17 13:20:46 +0300

    [MYYAR-18] Enable styled components

--- examples/InfrastructureExamples/webpack.config.client.js
+++ examples/InfrastructureExamples/webpack.config.client.js
@@ -21,7 +21,22 @@ module.exports = {
             },
             {
                 test: /\.css$/,
+                exclude: /\.module\.css$/,
                 use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.module\.css$/,
+                use: [
+                    require.resolve('style-loader'),
+                    {
+                        loader: require.resolve('css-loader'),
+                        options: {
+                            importLoaders: 1,
+                            modules: true,
+                            localIdentName: '[name]__[local]__[hash:base64:5]',
+                        },
+                    },
+                ],
             },
             {
                 test: /\.(png|woff|woff2|eot|ttf|svg)$/,
