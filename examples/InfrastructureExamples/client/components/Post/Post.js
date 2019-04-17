97098199e4a047b1064a3acc6847624f8696dd49
Konstantin Belousov <kombuchamp@gmail.com>
2019-04-17 18:16:55 +0300

    [MYYAR-18] Create ui for comments

--- examples/InfrastructureExamples/client/components/Post/Post.js
+++ examples/InfrastructureExamples/client/components/Post/Post.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import styles from './Post.module.css';
 
-export default function Post() {
+export default function Post(props) {
     return (
-        <div>
-            <span>Post</span>
+        <div className={styles.post}>
+            <p>
+                <b>{props.post.author}> </b>
+                {props.post.text}
+            </p>
         </div>
     );
 }
--- examples/InfrastructureExamples/client/components/Posts/Posts.js
+++ examples/InfrastructureExamples/client/components/Posts/Posts.js
@@ -2,29 +2,41 @@ import React, { Component } from 'react';
 import PostForm from '../PostForm/PostForm';
 import Post from '../Post/Post';
 import styles from './Posts.module.css';
+import axios from 'axios';
 
 export default class Posts extends Component {
     constructor(props) {
         super(props);
     }
     state = {
-        posts: [
-            {
-                id: 'guid',
-                author: 'test',
-                text: 'test',
-            },
-        ],
+        posts: [],
     };
+
+    loadPosts = () => {
+        axios.get('/api/get-posts').then(res => {
+            console.log('asdasd');
+            this.setState({ posts: res.data });
+        });
+    };
+
+    componentDidMount() {
+        this.loadPosts();
+    }
+
     render() {
         return (
             <div>
                 <h1>Posts</h1>
                 <div className={styles.container}>
                     {this.props.isAuthorized ? <PostForm /> : null}
-                    {this.state.posts.map(post => (
-                        <Post key={post.id} post={post} />
-                    ))}
+                    <button className={styles.button} onClick={this.loadPosts}>
+                        Refresh comments
+                    </button>
+                    <div className={styles.innerContainer}>
+                        {this.state.posts.map(post => (
+                            <Post key={post.id} post={post} />
+                        ))}
+                    </div>
                 </div>
             </div>
         );
