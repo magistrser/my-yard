a570fccb509ee44ac197a60290f353460c94ed03
Konstantin Belousov <kombuchamp@gmail.com>
2019-08-14 22:28:46 +0300

    [MYYAR-35] Add expansion panel to result list

--- examples/MapPostsExamples/client/components/Layout/ResultList.js
+++ examples/MapPostsExamples/client/components/Layout/ResultList.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 import { makeStyles, useTheme, withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import { List, ListItem, ListSubheader, ListItemText } from '@material-ui/core';
+import {
+    List,
+    ListItem,
+    ListSubheader,
+    ListItemText,
+    ExpansionPanel,
+    ExpansionPanelSummary,
+    ExpansionPanelDetails,
+    ExpansionPanelActions,
+} from '@material-ui/core';
 import Divider from '@material-ui/core/Divider';
 import { Card, CardContent, CardHeader } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 const useStyles = theme => ({
     searchResultsCard: {
@@ -27,39 +38,55 @@ class ResultList extends Component {
         const { classes, searchResults } = this.props;
 
         return (
-            <List style={{ width: '100%' }}>
-                <ListSubheader className={classes.resultListSubheader}>
-                    <Typography variant="h6" gutterBottom>
-                        Results:
-                    </Typography>
-                    <Divider />
-                </ListSubheader>
-                {searchResults.map(item => (
-                    <ListItem key={item.postId}>
-                        <ListItemText>
-                            <Card
-                                className={classes.searchResultsCard}
-                                raised={item.postId === this.state.selectedPostId}
-                                onClick={() => {
-                                    this.setState(
-                                        {
-                                            selectedPostId: item.postId,
-                                        },
-                                        () => this.props.onSearchResultClick(item.postId)
-                                    );
-                                }}
-                            >
-                                <CardHeader title={item.title} />
-                                <CardContent>
-                                    <Typography variant="body2" color="textSecondary" gutterBottom>
-                                        {item.description}
-                                    </Typography>
-                                </CardContent>
-                            </Card>
-                        </ListItemText>
-                    </ListItem>
-                ))}
-            </List>
+            <>
+                <Grid item>
+                    <ExpansionPanel>
+                        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>Options</ExpansionPanelSummary>
+                        <ExpansionPanelDetails>
+                            <Grid container direction="column">
+                                <Grid item>Foo</Grid>
+                                <Grid item>Bar</Grid>
+                                <Grid item>Baz</Grid>
+                            </Grid>
+                        </ExpansionPanelDetails>
+                    </ExpansionPanel>
+                </Grid>
+                <Grid item>
+                    <List style={{ width: '100%' }}>
+                        <ListSubheader className={classes.resultListSubheader}>
+                            <Typography variant="h6" gutterBottom>
+                                Results:
+                            </Typography>
+                            <Divider />
+                        </ListSubheader>
+                        {searchResults.map(item => (
+                            <ListItem key={item.postId}>
+                                <ListItemText>
+                                    <Card
+                                        className={classes.searchResultsCard}
+                                        raised={item.postId === this.state.selectedPostId}
+                                        onClick={() => {
+                                            this.setState(
+                                                {
+                                                    selectedPostId: item.postId,
+                                                },
+                                                () => this.props.onSearchResultClick(item.postId)
+                                            );
+                                        }}
+                                    >
+                                        <CardHeader title={item.title} />
+                                        <CardContent>
+                                            <Typography variant="body2" color="textSecondary" gutterBottom>
+                                                {item.description}
+                                            </Typography>
+                                        </CardContent>
+                                    </Card>
+                                </ListItemText>
+                            </ListItem>
+                        ))}
+                    </List>
+                </Grid>
+            </>
         );
     }
 }
