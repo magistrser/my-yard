acba4dc6c4d1df70db4433c784ca40a3fb0b84f0
Konstantin Belousov <kombuchamp@gmail.com>
2019-08-30 22:11:35 +0300

    [MYYAR-39] Build basic layout

--- examples/MapPostsExamples/client/components/Admin/Layout/Layout.js
+++ examples/MapPostsExamples/client/components/Admin/Layout/Layout.js
@@ -1,19 +1,46 @@
 import React, { Component } from 'react';
-import { CssBaseline, Toolbar, Drawer, ListItemIcon, ListItem, List, AppBar, Typography, ListItemText } from '@material-ui/core';
+import {
+    CssBaseline,
+    Toolbar,
+    Drawer,
+    ListItemIcon,
+    ListItem,
+    List,
+    AppBar,
+    Typography,
+    ListItemText,
+    Tabs,
+    Tab,
+    Divider,
+} from '@material-ui/core';
 import { Group as GroupIcon, Comment as CommentIcon, Assignment as AssignmentIcon } from '@material-ui/icons';
+import { withRouter, Link } from 'react-router-dom';
 
 const DRAWER_WIDTH = 180;
 
-export default class Layout extends Component {
-    state = {
-        selectedMainDrawerItem: null,
-    };
+class Layout extends Component {
+    constructor(props) {
+        super(props);
 
-    onMainDrawerButtonClick = ev => {
+        const path = props.location.pathname.split('/');
+        console.log(path); // TODO: deal with paths
+
+        this.state = {
+            selectedDrawerItem: null,
+            selectedTabValue: 0,
+        };
+    }
+
+    componentDidUpdate(prevProps) {
+        if (this.props.location !== prevProps.location) {
+            console.log('location updated ', this.props.location);
+        }
+    }
+
+    onDrawerButtonClick = ev => {
         const { id } = ev.currentTarget;
-        this.setState({
-            selectedMainDrawerItem: id,
-        });
+        console.log(id);
+        this.setState({ selectedDrawerItem: id, selectedTabValue: 0 });
     };
 
     render() {
@@ -21,18 +48,20 @@ export default class Layout extends Component {
             <div style={{ display: 'flex' }}>
                 <CssBaseline />
                 {this.appBar}
-                {this.mainDrawer}
-                <main style={{ flexGrow: 1, padding: 10 }}>
+                {this.drawer}
+                <div style={{ width: '100%' }}>
                     <div style={{ width: DRAWER_WIDTH, height: 64 }} />
-                    {this.props.children}
-                </main>
+                    <div style={{ height: 64, width: '100%' }}>{this.getTabs(this.state.selectedDrawerItem)}</div>
+                    <Divider />
+                    <main style={{ flexGrow: 1, padding: 10 }}>{this.props.children}</main>
+                </div>
             </div>
         );
     }
 
     get appBar() {
         return (
-            <AppBar position="fixed" style={{ zIndex: 2 }}>
+            <AppBar position="fixed" style={{ zIndex: 1201 }}>
                 <Toolbar>
                     <Typography variant="h6">Admin panel</Typography>
                 </Toolbar>
@@ -40,28 +69,18 @@ export default class Layout extends Component {
         );
     }
 
-    get mainDrawer() {
+    get drawer() {
         return (
-            <Drawer variant="permanent" style={{ width: DRAWER_WIDTH, flexShrink: 0, zIndex: 1 }}>
+            <Drawer variant="permanent" style={{ width: DRAWER_WIDTH, flexShrink: 0 }}>
                 <div style={{ width: DRAWER_WIDTH, height: 64 }} />
                 <List>
-                    <ListItem
-                        button
-                        id="users"
-                        selected={this.state.selectedMainDrawerItem == 'users'}
-                        onClick={this.onMainDrawerButtonClick}
-                    >
+                    <ListItem button id="users" selected={this.state.selectedDrawerItem == 'users'} onClick={this.onDrawerButtonClick}>
                         <ListItemIcon>
                             <GroupIcon />
                         </ListItemIcon>
                         <ListItemText primary="Users" />
                     </ListItem>
-                    <ListItem
-                        button
-                        id="posts"
-                        selected={this.state.selectedMainDrawerItem == 'posts'}
-                        onClick={this.onMainDrawerButtonClick}
-                    >
+                    <ListItem button id="posts" selected={this.state.selectedDrawerItem == 'posts'} onClick={this.onDrawerButtonClick}>
                         <ListItemIcon>
                             <AssignmentIcon />
                         </ListItemIcon>
@@ -70,8 +89,8 @@ export default class Layout extends Component {
                     <ListItem
                         button
                         id="comments"
-                        selected={this.state.selectedMainDrawerItem == 'comments'}
-                        onClick={this.onMainDrawerButtonClick}
+                        selected={this.state.selectedDrawerItem == 'comments'}
+                        onClick={this.onDrawerButtonClick}
                     >
                         <ListItemIcon>
                             <CommentIcon />
@@ -79,7 +98,45 @@ export default class Layout extends Component {
                         <ListItemText primary="Comments" />
                     </ListItem>
                 </List>
+                <Divider />
             </Drawer>
         );
     }
+
+    getTabs(currentDrawerOption) {
+        let label;
+
+        switch (currentDrawerOption) {
+            case 'users':
+                label = 'user';
+                break;
+            case 'posts':
+                label = 'post';
+                break;
+            case 'comments':
+                label = 'comment';
+                break;
+        }
+        console.log(label);
+
+        return (
+            label && (
+                <Tabs
+                    value={this.state.selectedTabValue}
+                    onChange={(ev, tabVal) => this.setState({ selectedTabValue: tabVal })}
+                    indicatorColor="primary"
+                    textColor="primary"
+                    variant="fullWidth"
+                >
+                    <Tab label={label + ' option 1'} />
+                    <Tab label={label + ' option 2'} />
+                    <Tab label={label + ' option 3'} />
+                    <Tab label={label + ' option 4'} />
+                    <Tab label={label + ' option 5'} />
+                </Tabs>
+            )
+        );
+    }
 }
+
+export default withRouter(Layout);
