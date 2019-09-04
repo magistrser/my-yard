27f3e8be25d0efb916fe641e8548d575fa020633
Konstantin Belousov <kombuchamp@gmail.com>
2019-09-04 16:04:03 +0300

    [MYYAR-39] Move admin page content to Dashbord comp

--- examples/MapPostsExamples/client/components/Admin/Admin.js
+++ examples/MapPostsExamples/client/components/Admin/Admin.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Layout from './Layout/Layout';
 import { Grid } from '@material-ui/core';
+import Dashboard from './Dashboard/Dashboard';
 
 export default class Admin extends Component {
     render() {
@@ -8,14 +9,7 @@ export default class Admin extends Component {
 
         return (
             <Layout>
-                <Grid container justify="flex-start" spacing={3} style={{ height: '100%' }}>
-                    <Grid item style={{ borderRight: '2px solid gray' }} xs={7}>
-                        <h1>Content</h1>
-                    </Grid>
-                    <Grid item xs={5}>
-                        <p>Selected user info, post preview, etc...</p>
-                    </Grid>
-                </Grid>
+                <Dashboard />
             </Layout>
         );
     }
