17e34d7b70bf497a273600b0e1eaedea9ac8ac31
Konstantin Belousov <kombuchamp@gmail.com>
2019-09-04 21:16:47 +0300

    [MYYAR-39] Add UserFilters comp

---
+++ examples/MapPostsExamples/client/components/Admin/Dashboard/Users/UserFilters/UserFilters.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import {
+    Grid,
+    Paper,
+    ExpansionPanel,
+    ExpansionPanelSummary,
+    Typography,
+    ExpansionPanelDetails,
+    List,
+    ListItem,
+    ListItemAvatar,
+    Avatar,
+    ListItemText,
+} from '@material-ui/core';
+import { ExpandMore as ExpandMoreIcon } from '@material-ui/icons';
+
+export default class UserFilters extends Component {
+    render() {
+        return (
+            <>
+                <ExpansionPanel>
+                    <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                        <Typography>Filters</Typography>
+                    </ExpansionPanelSummary>
+                    <ExpansionPanelDetails>
+                        <input />
+                    </ExpansionPanelDetails>
+                </ExpansionPanel>
+            </>
+        );
+    }
+}
