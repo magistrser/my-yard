707c2967eae201c6e41a3b2b24f465e38c3bd864
Konstantin Belousov <kombuchamp@gmail.com>
2019-09-06 17:34:45 +0300

    [MYYAR-39] Add BanControl comp

---
+++ examples/MapPostsExamples/client/components/Admin/Dashboard/Users/UserInfo/BanControl/BanControl.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { Button } from '@material-ui/core';
+
+export default class BanControl extends Component {
+    render() {
+        return <Button color="secondary">BAN</Button>;
+    }
+}
