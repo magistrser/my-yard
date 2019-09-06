887ec9c7326ef2bb2b2b1b08fdb553502900c846
Konstantin Belousov <kombuchamp@gmail.com>
2019-09-06 15:27:06 +0300

    [MYYAR-39] Add UserPosts component

---
+++ examples/MapPostsExamples/client/components/Admin/Dashboard/Users/UserPosts/UserPosts.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { Grid, Paper } from '@material-ui/core';
+
+export default class UserPosts extends Component {
+    render() {
+        return (
+            <Grid container>
+                <Grid item xs={6}>
+                    <Paper>
+                        <p>...Post filters</p>
+                    </Paper>
+                </Grid>
+                <Grid item xs={6}>
+                    <p>...Post list</p>
+                    <p>...Post list</p>
+                    <p>...Post list</p>
+                    <p>...Post list</p>
+                    <p>...Post list</p>
+                    <p>...Post list</p>
+                    <p>...Post list</p>
+                    <p>...Post list</p>
+                    <p>...Post list</p>
+                    <p>...Post list</p>
+                    <p>...Post list</p>
+                    <p>...Post list</p>
+                    <p>...Post list</p>
+                    <p>...Post list</p>
+                    <p>...Post list</p>
+                    <p>...Post list</p>
+                    <p>...Post list</p>
+                    <p>...Post list</p>
+                    <p>...Post list</p>
+                    <p>...Post list</p>
+                </Grid>
+            </Grid>
+        );
+    }
+}
