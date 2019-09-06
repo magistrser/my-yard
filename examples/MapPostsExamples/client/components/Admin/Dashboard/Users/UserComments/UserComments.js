77c8f9e2f9a2c20f1c3f63fdb17b8514e08a9636
Konstantin Belousov <kombuchamp@gmail.com>
2019-09-06 15:26:47 +0300

    [MYYAR-39] Add user comments

---
+++ examples/MapPostsExamples/client/components/Admin/Dashboard/Users/UserComments/UserComments.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { Grid, Paper } from '@material-ui/core';
+
+export default class UserComments extends Component {
+    render() {
+        return (
+            <Grid container>
+                <Grid item xs={6}>
+                    <Paper>
+                        <p>...Comment filters</p>
+                    </Paper>
+                </Grid>
+                <Grid item xs={6}>
+                    <p>...Comment list</p>
+                    <p>...Comment list</p>
+                    <p>...Comment list</p>
+                    <p>...Comment list</p>
+                    <p>...Comment list</p>
+                    <p>...Comment list</p>
+                    <p>...Comment list</p>
+                    <p>...Comment list</p>
+                    <p>...Comment list</p>
+                    <p>...Comment list</p>
+                    <p>...Comment list</p>
+                    <p>...Comment list</p>
+                    <p>...Comment list</p>
+                    <p>...Comment list</p>
+                    <p>...Comment list</p>
+                    <p>...Comment list</p>
+                    <p>...Comment list</p>
+                    <p>...Comment list</p>
+                    <p>...Comment list</p>
+                    <p>...Comment list</p>
+                </Grid>
+            </Grid>
+        );
+    }
+}
