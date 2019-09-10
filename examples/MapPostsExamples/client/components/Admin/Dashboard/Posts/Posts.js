b003ea8836cad2a7bfeff9d42ba787bfad4ed23d
Konstantin Belousov <kombuchamp@gmail.com>
2019-09-10 13:28:23 +0300

    [MYYAR-39] Make preview placeholder for post

--- examples/MapPostsExamples/client/components/Admin/Dashboard/Posts/Posts.js
+++ examples/MapPostsExamples/client/components/Admin/Dashboard/Posts/Posts.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
-import { Grid, Paper } from '@material-ui/core';
+import { Grid, Paper, Typography } from '@material-ui/core';
 import Post from '../../../Post/Post';
 
 export default class Posts extends Component {
     render() {
+        const { id } = this.props;
         return (
             <Grid container justify="flex-start" spacing={3} style={{ height: '100%', position: 'relative' }}>
                 <Grid item xs={4} style={{ height: '100%' }}>
                     <Paper style={{ height: '80%' }}>{/* <PostList /> */}</Paper>
                 </Grid>
-
                 <Grid item xs={8} style={{ height: '100%' }}>
                     <Paper style={{ height: '80%', position: 'relative' }}>
-                        {
-                            /* <PostPreview /> */
-                            <Post postId="69b7c7eb-8e44-4f24-9fa1-fcdf3e3773dc" />
-                        }
+                        {id ? (
+                            <Post postId={id} />
+                        ) : (
+                            <Typography
+                                variant="h4"
+                                color="textSecondary"
+                                style={{ position: 'relative', textAlign: 'center', top: '50%' }}
+                            >
+                                Select post to preview...
+                            </Typography>
+                        )}
                     </Paper>
                 </Grid>
             </Grid>
