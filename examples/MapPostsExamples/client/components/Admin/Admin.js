b908c7c0795a70ca0650fb1b40afd942091c56c9
Konstantin Belousov <kombuchamp@gmail.com>
2019-09-10 12:53:42 +0300

    [MYYAR-39] Put routes with ids directly into Admin

--- examples/MapPostsExamples/client/components/Admin/Admin.js
+++ examples/MapPostsExamples/client/components/Admin/Admin.js
@@ -2,13 +2,36 @@ import React, { Component } from 'react';
 import Layout from './Layout/Layout';
 import { Grid } from '@material-ui/core';
 import Dashboard from './Dashboard/Dashboard';
+import { Route } from 'react-router-dom';
+
+import Users from './Dashboard/Users/Users';
+import Posts from './Dashboard/Posts/Posts';
+//import Comments from "./Dashboard/Comments/Comments";
 
 export default class Admin extends Component {
+    routeComponent(component) {
+        return ({ match }) => {
+            const id = match?.params?.id;
+            return React.cloneElement(component, {
+                id,
+            });
+        };
+    }
+
     render() {
         console.log('---Admin renders');
         return (
             <Layout>
-                <Dashboard />
+                <Route path="/admin/users/:id?" exact render={this.routeComponent(<Users />)} />
+                <Route path="/admin/posts/:id?" exact render={this.routeComponent(<Posts />)} />
+                <Route
+                    path="/admin/comments/:id?"
+                    exact
+                    render={({ match }) => {
+                        const id = match?.params?.id;
+                        return <h1>comments {id}</h1>;
+                    }}
+                />
             </Layout>
         );
     }
