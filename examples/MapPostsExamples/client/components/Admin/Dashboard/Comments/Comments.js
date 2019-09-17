cf7712c851752025bfaccb1f37d18bc3f3bdc2e4
Konstantin Belousov <kombuchamp@gmail.com>
2019-09-17 13:40:33 +0300

    [MYYAR-39] Move comment list into right position

--- examples/MapPostsExamples/client/components/Admin/Dashboard/Comments/Comments.js
+++ examples/MapPostsExamples/client/components/Admin/Dashboard/Comments/Comments.js
@@ -14,18 +14,22 @@ import {
 } from '@material-ui/core';
 import { ExpandMore as ExpandMoreIcon } from '@material-ui/icons';
 
+import CommentsList from './CommentsList/CommentsList';
+
 class Comments extends Component {
     render() {
         const { id } = this.props;
 
         return (
             <Grid container justify="flex-start" spacing={3} style={{ height: '100%', position: 'relative' }}>
-                <Grid item xs={4} style={{ height: '100%' }}>
-                    <Paper style={{ height: '80%' }}>Comment list</Paper>
+                <Grid item xs={6} style={{ height: '100%' }}>
+                    <Paper style={{ height: '80%' }}>
+                        <CommentsList />
+                    </Paper>
                 </Grid>
 
-                <Grid item xs={8} style={{ height: '100%' }}>
-                    <Paper style={{ height: '80%', position: 'relative' }}>Comment details</Paper>
+                <Grid item xs={6} style={{ height: '100%' }}>
+                    <Paper style={{ height: '80%', position: 'relative' }}>asdf</Paper>
                 </Grid>
             </Grid>
         );
