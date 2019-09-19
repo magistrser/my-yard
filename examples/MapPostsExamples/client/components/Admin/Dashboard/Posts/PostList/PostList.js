b13f8721792ab24592ad06976c155295fb5dcdd8
Konstantin Belousov <kombuchamp@gmail.com>
2019-09-19 12:35:52 +0300

    [MYYAR-39] Pass posts through props

--- examples/MapPostsExamples/client/components/Admin/Dashboard/Posts/PostList/PostList.js
+++ examples/MapPostsExamples/client/components/Admin/Dashboard/Posts/PostList/PostList.js
@@ -1,34 +1,32 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Grid, List, MenuItem, ListItemAvatar, Avatar, ListItemText } from '@material-ui/core';
+import { Grid, List, MenuItem, ListItemAvatar, Avatar, ListItemText, Paper } from '@material-ui/core';
 import PostFilters from '../PostFilters/PostFilters';
 
 export default class PostList extends Component {
     render() {
         const selectedPostId = this.props.id;
         return (
-            <Grid container direction="column" style={{ height: '100%', flexWrap: 'nowrap' }}>
-                <Grid item>
-                    <PostFilters />
+            <Paper style={{ width: '100%' }}>
+                <Grid container direction="column" style={{ height: '100%', flexWrap: 'nowrap' }}>
+                    <Grid item>
+                        <PostFilters />
+                    </Grid>
+                    <Grid item style={{ overflowY: 'auto' }}>
+                        <List dense>
+                            {this.props.posts?.map(post => {
+                                return (
+                                    <Link key={post.id} to={`/admin/posts/${post.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                                        <MenuItem button selected={post.id == selectedPostId}>
+                                            <ListItemText primary={'Title ' + post.title} secondary={post.text.slice(0, 50)} />
+                                        </MenuItem>
+                                    </Link>
+                                );
+                            })}
+                        </List>
+                    </Grid>
                 </Grid>
-                <Grid item style={{ overflowY: 'auto' }}>
-                    <List dense>
-                        {[
-                            'b07a0157-df23-4839-8116-13b73807ade2',
-                            'f24c4380-7cb6-4f6b-b3e6-e0df34fcdc09',
-                            '22421e2c-beb6-4e02-9d24-146c81eb9f7e',
-                        ].map(val => {
-                            return (
-                                <Link key={val} to={`/admin/posts/${val}`} style={{ textDecoration: 'none', color: 'inherit' }}>
-                                    <MenuItem button selected={val == selectedPostId}>
-                                        <ListItemText primary={'Title ' + val} secondary={'Text preview...'} />
-                                    </MenuItem>
-                                </Link>
-                            );
-                        })}
-                    </List>
-                </Grid>
-            </Grid>
+            </Paper>
         );
     }
 }
